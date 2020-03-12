d501d1fb32977644c8d70b561835d7396b475db2
Naman Chikara <namanc@sahajsoft.com>
2020-03-12 18:15:06 +0530

    Switched gtts

--- voicemail/TextToSpeech.py
+++ voicemail/TextToSpeech.py
@@ -1,9 +1,11 @@
 import hashlib
 import os
 import tempfile
+import time
 
 import pyglet
 import pyttsx3
+from gtts import gTTS
 
 from config import LanguageConfig
 
@@ -13,14 +15,17 @@ class TextToSpeech:
         self.text = text
         self.lang = language
         self.file_address = self.__build_file_address()
-        # self.tts = gTTS(text=self.text, lang=self.lang)
-        # self.__save_to_file()
+        self.tts = gTTS(text=self.text, lang=self.lang)
+        self.__save_to_file()
+        speech = self.__to_speech()
+        speech.play()
+        time.sleep(speech.duration)
 
-        # self.tts.setProperty('voice', self.voices[1].id)
-        self.tts = pyttsx3.init()
-        # self.voices = self.tts.getProperty('voices')  # getting details of current voice
-        self.tts.say(self.text)
-        self.tts.runAndWait()
+        # # self.tts.setProperty('voice', self.voices[1].id)
+        # self.tts = pyttsx3.init()
+        # # self.voices = self.tts.getProperty('voices')  # getting details of current voice
+        # self.tts.say(self.text)
+        # self.tts.runAndWait()
 
     def __build_file_address(self):
         return os.path.join(
@@ -32,11 +37,9 @@ class TextToSpeech:
         return pyglet.media.load(self.file_address, streaming=False)
 
     def speak_out_loud(self):
-        # speech = self.__to_speech()
-        # speech.play()
-        # time.sleep(speech.duration)
+
         pass
 
-    # def __save_to_file(self):
-    #     if not os.path.exists(self.file_address):
-    #         self.tts.save(self.file_address)
+    def __save_to_file(self):
+        if not os.path.exists(self.file_address):
+            self.tts.save(self.file_address)
